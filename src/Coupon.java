4aa787f5b5a849eee39c4c0944649b9e989f68f4
maritzabeltran <maritzabeltran@gustavus.edu>
2024-12-18 14:25:40 -0600

    writing main and coupon class

---
+++ src/Coupon.java
@@ -0,0 +1,11 @@
+public class Coupon {
+    private String dateAccessed;
+    private Integer couponId;
+    private String couponName;
+    private Boolean used;
+
+    public static void main(String[] args){
+
+    }
+
+}
